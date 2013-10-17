4980385b16b85c14de23ca82a1b15d0b7123a717
zhangchunsheng <zhangchunsheng423@gmail.com>
2013-10-17 10:06:20 +0800

    1、新功能：初始化项目

--- protectProcess.sh
+++ protectProcess.sh
@@ -1,6 +1,6 @@
 cd /home/hacker
 path=/home/hacker/logs
-proc_num=`ps -ef |awk '{print $9}'|grep a.php|grep -v grep|wc -l`
+proc_num=`ps -ef |grep process info|grep -v grep|awk '{print $10}'|wc -l`
 date=`date +'%Y-%m-%d'`
 
 if [ $proc_num -ge 1 ]
@@ -11,3 +11,19 @@ else
 	nohup /usr/local/php-5.3.6/bin/php a.php 2>&1 > /dev/null &
 	echo `date +'%Y-%m-%d %H:%M:%S'` a.php is restart! >> $path/process$date.txt
 fi
+
+Comparisons:
+-eq	equal to
+-ne	not equal to
+-lt	less than
+-le	less than or equal to
+-gt	greater than
+-ge	greater than or equal to
+
+File Operations:
+-s	file exists and is not empty
+-f	file exists and is not a directory
+-d	directory exists
+-x	file is executable
+-w	file is writable
+-r	file is readable
