417c9a900e3227de54a26f99b899aa1465625e8c
zhangchunsheng <zhangchunsheng423@gmail.com>
2013-10-17 10:22:44 +0800

    1、新功能：新增监控nodejs进程

--- app/wozlla/seaking_server/nodeprocess.sh
+++ app/wozlla/seaking_server/nodeprocess.sh
@@ -1,13 +1,13 @@
-cd /home/hacker
-path=/home/hacker/logs
+cd /home/wozlla/seaking_server
+path=/home/wozlla/seaking_server/logs
 proc_num=`ps -ef |grep port=4012|grep -v grep|awk '{print $10}'|wc -l`
 date=`date +'%Y-%m-%d'`
 
 if [ $proc_num -ge 1 ]
 then
-	echo `date +'%Y-%m-%d %H:%M:%S'` seaking_server port=4012 running ok! >> $path/process$date.txt
+	echo `date +'%Y-%m-%d %H:%M:%S'` port=4012 seaking_server running ok! >> $path/process$date.txt
 else
-	echo `date +'%Y-%m-%d %H:%M:%S'` seaking_server port=4012 is stopped! >> $path/process$date.txt
-	nohup node app.js 2>&1 > /dev/null &
-	echo `date +'%Y-%m-%d %H:%M:%S'` seaking_server port=4012 is restart! >> $path/process$date.txt
+	echo `date +'%Y-%m-%d %H:%M:%S'` port=4012 seaking_server is stopped! >> $path/process$date.txt
+	nohup /usr/local/bin/node app.js port=4012 seaking_server 2>&1 > /dev/null &
+	echo `date +'%Y-%m-%d %H:%M:%S'` port=4012 seaking_server is restart! >> $path/process$date.txt
 fi
